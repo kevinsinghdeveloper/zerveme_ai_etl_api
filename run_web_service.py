d09a3576c06d67aa0323a3a10f1c99c62c3cb5da
kevin singh <kevinsingh.developer@gmail.com>
2024-11-07 20:21:19 -0500

    REC-1 todo

--- run_web_service.py
+++ run_web_service.py
@@ -25,3 +25,9 @@ def run_web_service():
 
 if __name__ == '__main__':
     run_web_service()
+
+# TODO
+#   Adjust model to match the config, title etc.
+#   fix interfaces, not strict as c#
+#   setup and complete resource manager with request to serp
+#   add finishing pieces to rec -> we should manager a model, which will become a table
