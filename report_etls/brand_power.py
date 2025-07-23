688b9ccc894489c224729d5bd3a08ab4bd5e9373
kevin singh <kevinsingh.developer@gmail.com>
2025-07-22 22:16:27 -0400

    checkpoint

--- report_etls/brand_power.py
+++ report_etls/brand_power.py
@@ -2,11 +2,14 @@ import logging
 from abstractions.EtlReportBase import EtlReportBase
 from abstractions.ILLMServiceManager import ILLMServiceManager
 
+INDUSTRIES = {}
 
 class BrandPower(EtlReportBase):
     def __init__(self, run_params: dict, llm_service_manager: ILLMServiceManager):
         super().__init__(run_params, "brand_power", llm_service_manager)
         # run params should contain ai configuration
+        self.__report_name = None
+        self.__report_id = None
 
     def configure_init_tasks(self):
         self._pre_validation_pipeline_tasks  = {
